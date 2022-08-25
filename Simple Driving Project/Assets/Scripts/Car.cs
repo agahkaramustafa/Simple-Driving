835bab9132ffd0d6aaabed01b22ec5653466cb6b
Agah Karamustafa <96539896+agahkaramustafa@users.noreply.github.com>
2022-08-25 16:26:28 +0300

    Camera follow

--- Simple Driving Project/Assets/Scripts/Car.cs
+++ Simple Driving Project/Assets/Scripts/Car.cs
@@ -6,10 +6,19 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float acc = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
+
+    private int steerValue;
     // Update is called once per frame
     void Update()
     {
         speed += acc * Time.deltaTime;
+        transform.Rotate(0f, steerValue * turnSpeed * Time.deltaTime, 0f);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
